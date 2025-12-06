e275fcfe737c69ab0d7837b5b186e23800ee77c7
1lelanta <kekikoki4@gmail.com>
2025-12-06 09:49:12 +0300

    add open ai apie for  resume analysisis

---
+++ server/controllers/analysisController.js
@@ -0,0 +1,23 @@
+import {OpenAI} from 'apenai';
+import { model } from 'mongoose';
+
+const client = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+
+export const analyzeResume = async(req,res)=>{
+    try {
+        const path = req.file.path;
+
+        const response = await client.chat.completions.create({
+            model: "gpt-5-nano",
+            message: [
+                {role:"system", content:"You are a resume analysis AI."},
+                {role: "user", content:`Analyze this resume file path: ${path}`}
+            ]
+        });
+
+        res.json({analysis:response.choices[0].message.content});
+        
+    } catch (err) {
+        res.status(500).json({error:err.message});
+    }
+}
